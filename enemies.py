bb19c724219e99169eb67f653df321ac9bd8c70a
unknown <kauancdmoraes1@gmail.com>
2023-07-19 23:09:55 -0300

    Update enemies.py

--- enemies.py
+++ enemies.py
@@ -14,7 +14,16 @@ class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.initial_pos = pos
-        self.frame.index=
+        self.frame.index  =0
+        self.image = self.animations['Idle'][self.frame_index]
+        self.rect = self.image.get_rect(topleft=pos)
+        self.direction = pygame.math.Vector2(0, 0)
+        self.speed = 4
+        self.gravity = 0.6
+        self.status = 'Idle'
         
-        
-        
+    def move(self):
+        if self.status=='Idle':
+            if random.randint(0,30)==2:
+                self.direction*=-1
+                
