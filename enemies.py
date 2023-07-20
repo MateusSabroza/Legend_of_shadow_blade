97ac7cb813150f31d9f8702c8a02b0c21bdb0f4c
BernardoSGouveia <137237329+BernardoSGouveia@users.noreply.github.com>
2023-07-19 23:36:24 -0300

    enemies.py

--- enemies.py
+++ enemies.py
@@ -1,29 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul 19 22:17:12 2023
-
-@author: Kauan
-"""
-
 import pygame
-from sys import exit
 from settings import *
-from os import walk
+from player import import_folder
+import random
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.initial_pos = pos
-        self.frame.index  =0
+        self.import_character_assets()
+        self.frame_index = 0
         self.image = self.animations['Idle'][self.frame_index]
         self.rect = self.image.get_rect(topleft=pos)
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 4
         self.gravity = 0.6
         self.status = 'Idle'
+        self.attack_bool = False
         
+    def import_character_assets(self):
+        character_path = 'graphics//Enemies//enemy//'
+        self.animations = {'Idle':[],'Run':[],'Jump':[],'Fall':[], 'Attack1':[], 'Attack2':[], 'Death':[], 'Take hit': []}
+        
+        for animation in self.animations.keys():
+            full_path = character_path + animation
+            self.animations[animation] = import_folder(full_path)
+    
+    def apply_gravity(self):
+        # metodo para aplicar a gravidade
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
     def move(self):
+        self.apply_gravity()
         if self.status=='Idle':
             if random.randint(0,30)==2:
                 self.direction*=-1
+    
+    def update(self, x_shift):
+        self.rect.x += x_shift
+        self.move()
                 
