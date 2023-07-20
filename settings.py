1662c5a33a6d67e9d061697a8e7adab27f4ba6b1
BernardoSGouveia <137237329+BernardoSGouveia@users.noreply.github.com>
2023-07-20 01:34:40 -0300

    settings.py

--- settings.py
+++ settings.py
@@ -2,15 +2,15 @@ level_map = [
     '                                                                                                             ',
     ' A                                                                                                           ',
     'x                                                                                                           x',    
-    'x       A  I  A  A  A                                                                                       x',
+    'x          I  A  A  A      I                                                                                x',
     'x A      xxxxx    xx     A  A                A A     A  A                                                   x',
     'xx             x          xx                  x       xx   A       A                                        x',
     'x                         xx                  x       xx    xxxxxxx                                         x',
     'x A    P    A  A  I  A    xx                  x       xx               A   A   A       A                    x',
     'x  xxxxxxxxx    xxxxx     xx                  x       xx                xxx     xxxxxxx                     x',
     'x A                       xx                  x       xx                                                    x',
-    'xx A       A A    A  A   AxxA      A A A A                                                                   ',
-    'xxxA A A A AxA A A xx     xx    A   x   x                  A      A              A AA A   A A   A A A A      ',
+    'xx A       A A       A   AxxA      A A A A                                                                   ',
+    'xxxA A A A AxA A   xx     xx    A   x   x                  A      A              A AA A   A A   A A A A      ',
     'xxx x x x x x x x xxxxxxxxx xxxx                            xxxxxx                x  x xxx   xxx   x   xxxxxx',
 ]
 tile_size = 64
